1a694b4a56fd527bd23ec6b01addf58888303476
voncuver <sierpios@op.pl>
2013-04-25 22:35:35 +0200

    Automatic wire

--- src/main/java/com/pk/cwierkacz/processor/handlers/WireTweetAccount.java
+++ src/main/java/com/pk/cwierkacz/processor/handlers/WireTweetAccount.java
@@ -1,16 +1,13 @@
 package com.pk.cwierkacz.processor.handlers;
 
-import java.io.BufferedOutputStream;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.pk.cwierkacz.http.Action;
 import com.pk.cwierkacz.http.Status;
@@ -22,6 +19,7 @@ import com.pk.cwierkacz.model.dao.TwitterAccountDao;
 import com.pk.cwierkacz.model.dao.UserDao;
 import com.pk.cwierkacz.model.service.ServiceRepo;
 import com.pk.cwierkacz.model.service.UserService;
+import com.pk.cwierkacz.processor.handlers.halpers.HttpClient;
 import com.pk.cwierkacz.twitter.OAuthAuthentication;
 import com.pk.cwierkacz.twitter.TwitterAuthenticationException;
 
@@ -30,8 +28,11 @@ public class WireTweetAccount implements Handler
 
     private final UserService userService;
 
+    private final HttpClient httpClient;
+
     public WireTweetAccount() {
         userService = ServiceRepo.getInstance().getService(UserService.class);
+        httpClient = new HttpClient();
     }
 
     @Override
@@ -78,48 +79,43 @@ public class WireTweetAccount implements Handler
             userAuthentication = new OAuthAuthentication(account);
             map.put(accRequest.getTokenId(), userAuthentication);
         }
-
+        String pin = null;
         try {
-            //String sessionUrl = "authenticity_token=%s&oauth_token=%s&username_or_email=%s&password=%s";
+            String url = userAuthentication.getAuthenticationURL();
 
-            if ( accRequest.getPin() == null ) {
+            String authUrl = "https://api.twitter.com/oauth/authorize";
 
-                String url = userAuthentication.getAuthenticationURL();
-                Response response = ResponseImpl.create(Status.OK, url, accRequest.getTokenId());
-                appData.setResponse(response);
-                return;
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(httpClient.getUrlConnection(url,
+                                                                                                                      false,
+                                                                                                                      null)
+                                                                                                    .getInputStream()),
+                                                                  HttpClient.CHARSET_UTF8));
+
+                String token = extractToken(reader);
+                String outhToken = url.split("=")[ 1 ];
+                String postData = "authenticity_token=" +
+                                  token +
+                                  "&oauth_token=" +
+                                  outhToken +
+                                  "&session%5Busername_or_email%5D=" +
+                                  accRequest.getAccountTweet() +
+                                  "&session%5Bpassword%5D=" +
+                                  accRequest.getPassTweet();
+
+                reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(httpClient.getUrlConnection(authUrl,
+                                                                                                                      true,
+                                                                                                                      postData)
+                                                                                                    .getInputStream()),
+                                                                  HttpClient.CHARSET_UTF8));
+                pin = extractPin(reader);
+                reader.close();
             }
-            else {
-
-                Long pin = accRequest.getPin();
-                account = userAuthentication.authenticate(pin.toString(), true);
+            catch ( IOException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-
-            //            
-            //            String authUrl = "https://api.twitter.com/oauth/authorize";
-            //
-            //            BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(getUrlConnection(url,
-            //                                                                                                                      false,
-            //                                                                                                                      null).getInputStream()),
-            //                                                                             "UTF-8"));
-            //            String token = extractToken(reader);
-            //            String outhToken = url.split("=")[ 1 ];
-            //            String postData = String.format(sessionUrl,
-            //                                            token,
-            //                                            outhToken,
-            //                                            accRequest.getAccountTweet(),
-            //                                            accRequest.getPassTweet());
-            //            authUrl = authUrl + "?" + postData;
-            //            reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(getUrlConnection(authUrl,
-            //                                                                                                       true,
-            //                                                                                                       null).getInputStream()),
-            //                                                              "UTF-8"));
-            //            String line;
-            //            StringBuilder stringBuilder = new StringBuilder();
-            //            while ( null != ( line = reader.readLine() ) ) {
-            //                stringBuilder.append(line);
-            //            }
-            //            reader.close();
 
         }
         catch ( TwitterAuthenticationException e ) {
@@ -127,6 +123,15 @@ public class WireTweetAccount implements Handler
             appData.setResponse(response);
             return;
         }
+
+        try {
+            account = userAuthentication.authenticate(pin, true);
+        }
+        catch ( TwitterAuthenticationException e ) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         user.getAccounts().add(account);
         userService.saveOrUpdate(user);
 
@@ -135,6 +140,20 @@ public class WireTweetAccount implements Handler
                                                 accRequest.getTokenId());
         appData.setResponse(response);
         return;
+    }
+
+    private String extractPin( BufferedReader reader ) throws IOException {
+        String pin = null;
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ( null != ( line = reader.readLine() ) ) {
+            stringBuilder.append(line);
+            if ( line.contains("<code>") ) {
+                pin = line.split("[><]")[ 4 ];
+            }
+        }
+        return pin;
     }
 
     private String extractToken( BufferedReader reader ) throws IOException {
@@ -154,100 +173,6 @@ public class WireTweetAccount implements Handler
             }
         }
         return null;
-    }
-
-    public final Map<String, String> connectionHeaders = new HashMap<String, String>();
-    public static final Charset CHARSET_UTF8 = Charset.forName("UTF-8");
-    private final StringBuffer cookie = new StringBuffer(512);
-    public String lastLocation = "";
-
-    public final HttpURLConnection getUrlConnection( final String url, final boolean post, final String output ) throws IOException {
-        int retries = 0;
-        HttpURLConnection conn;
-        while ( true ) {
-            try {
-                final URL urlObj = new URL(url);
-                conn = (HttpURLConnection) urlObj.openConnection();
-                conn.setConnectTimeout(30 * 1000);
-                conn.setReadTimeout(5 * 60 * 1000);
-                if ( post ) {
-                    conn.setRequestMethod("POST");
-                }
-                final String referer;
-                final int pathIdx;
-                if ( ( pathIdx = url.lastIndexOf('/') ) > "https://".length() ) {
-                    referer = url.substring(0, pathIdx);
-                }
-                else {
-                    referer = url;
-                }
-                conn.setRequestProperty("Referer", referer);
-                final Set<String> keys = this.connectionHeaders.keySet();
-                for ( final String k : keys ) {
-                    final String value = this.connectionHeaders.get(k);
-                    if ( value != null ) {
-                        conn.setRequestProperty(k, value);
-                    }
-                }
-                conn.setUseCaches(false);
-                if ( output != null ) {
-                    conn.setDoOutput(true);
-                    final BufferedOutputStream out = new BufferedOutputStream(conn.getOutputStream());
-                    out.write(output.getBytes(CHARSET_UTF8));
-                    out.close();
-                }
-                if ( appendCookies(this.cookie, conn) ) {
-                    putConnectionHeader("Cookie", this.cookie.toString());
-                }
-                break;
-            }
-            catch ( final Throwable e ) {
-                e.printStackTrace();
-                if ( retries++ > 20 ) {
-                    throw new IOException(e);
-                }
-                else {
-                    try {
-                        Thread.sleep(( 60 * retries * 100 ) + ( (int) Math.random() * 100 * 60 * retries ));
-                    }
-                    catch ( final InterruptedException e1 ) {
-                        e1.printStackTrace();
-                    }
-                }
-            }
-        }
-        return conn;
-    }
-
-    public final boolean appendCookies( final StringBuffer cookie, final HttpURLConnection conn ) throws IOException {
-        try {
-            boolean changed = false;
-            final Map<String, List<String>> headerFields = conn.getHeaderFields();
-            this.lastLocation = conn.getHeaderField("Location");
-            if ( this.lastLocation == null ) {
-                this.lastLocation = "";
-            }
-            final List<String> values = headerFields.get("Set-Cookie");
-            if ( values != null ) {
-                for ( final String v : values ) {
-                    if ( v.indexOf("deleted") == -1 ) {
-                        if ( cookie.length() > 0 ) {
-                            cookie.append("; ");
-                        }
-                        cookie.append(v.split(";")[ 0 ]);
-                        changed = true;
-                    }
-                }
-            }
-            return changed;
-        }
-        catch ( final Throwable e ) {
-            throw new IOException(e);
-        }
-    }
-
-    public final void putConnectionHeader( final String key, final String value ) {
-        this.connectionHeaders.put(key, value);
     }
 
 }
---
+++ src/main/java/com/pk/cwierkacz/processor/handlers/halpers/HttpClient.java
@@ -0,0 +1,110 @@
+package com.pk.cwierkacz.processor.handlers.halpers;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class HttpClient
+{
+
+    public final Map<String, String> connectionHeaders = new HashMap<String, String>();
+    public static final Charset CHARSET_UTF8 = Charset.forName("UTF-8");
+    private final StringBuffer cookie = new StringBuffer(512);
+    public String lastLocation = "";
+
+    public final HttpURLConnection getUrlConnection( final String url, final boolean post, final String output ) throws IOException {
+        int retries = 0;
+        HttpURLConnection conn;
+        while ( true ) {
+            try {
+                final URL urlObj = new URL(url);
+                conn = (HttpURLConnection) urlObj.openConnection();
+                conn.setConnectTimeout(30 * 1000);
+                conn.setReadTimeout(5 * 60 * 1000);
+                if ( post ) {
+                    conn.setRequestMethod("POST");
+                }
+                final String referer;
+                final int pathIdx;
+                if ( ( pathIdx = url.lastIndexOf('/') ) > "https://".length() ) {
+                    referer = url.substring(0, pathIdx);
+                }
+                else {
+                    referer = url;
+                }
+                conn.setRequestProperty("Referer", referer);
+                final Set<String> keys = this.connectionHeaders.keySet();
+                for ( final String k : keys ) {
+                    final String value = this.connectionHeaders.get(k);
+                    if ( value != null ) {
+                        conn.setRequestProperty(k, value);
+                    }
+                }
+                conn.setUseCaches(false);
+                if ( output != null ) {
+                    conn.setDoOutput(true);
+                    final BufferedOutputStream out = new BufferedOutputStream(conn.getOutputStream());
+                    out.write(output.getBytes(CHARSET_UTF8));
+                    out.close();
+                }
+                if ( appendCookies(this.cookie, conn) ) {
+                    putConnectionHeader("Cookie", this.cookie.toString());
+                }
+                break;
+            }
+            catch ( final Throwable e ) {
+                e.printStackTrace();
+                if ( retries++ > 20 ) {
+                    throw new IOException(e);
+                }
+                else {
+                    try {
+                        Thread.sleep(( 60 * retries * 100 ) + ( (int) Math.random() * 100 * 60 * retries ));
+                    }
+                    catch ( final InterruptedException e1 ) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
+        return conn;
+    }
+
+    public final boolean appendCookies( final StringBuffer cookie, final HttpURLConnection conn ) throws IOException {
+        try {
+            boolean changed = false;
+            final Map<String, List<String>> headerFields = conn.getHeaderFields();
+            this.lastLocation = conn.getHeaderField("Location");
+            if ( this.lastLocation == null ) {
+                this.lastLocation = "";
+            }
+            final List<String> values = headerFields.get("Set-Cookie");
+            if ( values != null ) {
+                for ( final String v : values ) {
+                    if ( v.indexOf("deleted") == -1 ) {
+                        if ( cookie.length() > 0 ) {
+                            cookie.append("; ");
+                        }
+                        cookie.append(v.split(";")[ 0 ]);
+                        changed = true;
+                    }
+                }
+            }
+            return changed;
+        }
+        catch ( final Throwable e ) {
+            throw new IOException(e);
+        }
+    }
+
+    public final void putConnectionHeader( final String key, final String value ) {
+        this.connectionHeaders.put(key, value);
+    }
+
+}
