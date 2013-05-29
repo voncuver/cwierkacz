361f9b8773ace4a5394c4499b62c4a57ef59f016
voncuver <sierpios@op.pl>
2013-05-29 23:15:29 +0200

    some changes

--- src/test/java/com/pk/cwierkacz/ws/ServiceTest.java
+++ src/test/java/com/pk/cwierkacz/ws/ServiceTest.java
@@ -3,25 +3,22 @@ package com.pk.cwierkacz.ws;
 import org.junit.Test;
 
 import pl.edu.pk.ias.socialserviceintegration.InvalidLoginOrPasswordFault;
-import pl.edu.pk.ias.socialserviceintegration.SocialServiceIntegration;
+import pl.edu.pk.ias.socialserviceintegration.SocialServiceIntegration_Service;
 import pl.edu.pk.ias.types.LoginRequest;
 import pl.edu.pk.ias.types.LoginResponse;
-
-import com.pk.cwierkacz.ws.client.SocialServiceIntegrationImplService;
 
 public class ServiceTest
 {
     @Test
     public void simpleTest( ) throws InvalidLoginOrPasswordFault {
-        SocialServiceIntegrationImplService implService = new SocialServiceIntegrationImplService("https://37.28.156.233:8181/FilckrIAS/services/integration/socialserviceintegration");
-
-        SocialServiceIntegration ssiPort = implService.getSocialServiceIntegrationImplPort();
+        SocialServiceIntegration_Service implService = new SocialServiceIntegration_Service();
 
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setLogin("Test");
         loginRequest.setPassword("Test");
 
-        LoginResponse loginResponse = ssiPort.login(loginRequest);
+        LoginResponse loginResponse = implService.getSocialServiceIntegrationEndpoint().login(loginRequest);
+
         System.out.println(loginResponse.getToken());
     }
 
