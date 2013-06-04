5a531d054f9f938de856d8e4a20eb135bb0c6be2
radek <twardy.r@gmail.com>
2013-06-04 21:11:44 +0200

    fix equals method in UserDao

--- src/main/java/com/pk/cwierkacz/model/dao/UserDao.java
+++ src/main/java/com/pk/cwierkacz/model/dao/UserDao.java
@@ -121,60 +121,18 @@ public class UserDao implements Serializable
     }
 
     @Override
-    public int hashCode( ) {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ( ( accounts == null ) ? 0 : accounts.hashCode() );
-        result = prime * result + ( ( id == null ) ? 0 : id.hashCode() );
-        result = prime * result + ( isDeleted ? 1231 : 1237 );
-        result = prime * result + ( ( name == null ) ? 0 : name.hashCode() );
-        result = prime * result + ( ( password == null ) ? 0 : password.hashCode() );
-        result = prime * result + ( ( session == null ) ? 0 : session.hashCode() );
-        return result;
-    }
-
-    @Override
     public boolean equals( Object obj ) {
         if ( this == obj )
             return true;
-        if ( obj == null )
+        if ( ( obj == null ) || ( obj.getClass() != this.getClass() ) )
             return false;
-        if ( getClass() != obj.getClass() )
-            return false;
-        UserDao other = (UserDao) obj;
-        if ( accounts == null ) {
-            if ( other.accounts != null )
-                return false;
-        }
-        else if ( !accounts.equals(other.accounts) )
-            return false;
-        if ( id == null ) {
-            if ( other.id != null )
-                return false;
-        }
-        else if ( !id.equals(other.id) )
-            return false;
-        if ( isDeleted != other.isDeleted )
-            return false;
-        if ( name == null ) {
-            if ( other.name != null )
-                return false;
-        }
-        else if ( !name.equals(other.name) )
-            return false;
-        if ( password == null ) {
-            if ( other.password != null )
-                return false;
-        }
-        else if ( !password.equals(other.password) )
-            return false;
-        if ( session == null ) {
-            if ( other.session != null )
-                return false;
-        }
-        else if ( !session.equals(other.session) )
-            return false;
-        return true;
+        UserDao userDao = (UserDao) obj;
+        return userDao.getName().equals(name);
+    }
+
+    @Override
+    public int hashCode( ) {
+        return name.hashCode();
     }
 
 }
